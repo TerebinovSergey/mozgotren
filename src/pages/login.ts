d68204f11601b647542312f2d07e65a21f29cc6d
Alex Leoniuk <alex.brest.by@gmail.com>
2023-02-02 20:37:19 +0300

    feat: login/register pages add

--- src/pages/login.ts
+++ src/pages/login.ts
@@ -32,11 +32,43 @@ export const loginPage = async () => {
 
   const loginBoxBody = loginForm('login');
 
-  // const result = await fetch(`${baseUrl}/users`, {
-  //   method: 'GET'
-  // });
-  // const users = await result.json();
-  // console.log(users);
+  const result = await fetch(`${baseUrl}/users`, {
+    method: 'GET'
+  });
+  const users = await result.json();
+  console.log(users);
+
+  const loginBoxBodyRow = document.createElement('div');
+  loginBoxBodyRow.classList.add('row', 'row-login');
+
+  const loginBoxBodyCheckboxWrapper = document.createElement('div');
+  loginBoxBodyCheckboxWrapper.classList.add('checkbox-wrapper');
+
+  const loginBoxBodyLabel = document.createElement("Label");
+  loginBoxBodyLabel.setAttribute("for", "login-checkbox");
+  loginBoxBodyLabel.innerHTML = "Запомнить меня";
+
+  const loginBoxBodyCheckbox = document.createElement('input');
+  loginBoxBodyCheckbox.classList.add('login-checkbox');
+  loginBoxBodyCheckbox.id = 'login-checkbox';
+  loginBoxBodyCheckbox.type = 'checkbox';
+
+  loginBoxBodyCheckboxWrapper.append(loginBoxBodyCheckbox, loginBoxBodyLabel);
+
+  const loginBoxBodyButton = document.createElement('div');
+  loginBoxBodyButton.classList.add('login-button');
+  loginBoxBodyButton.innerHTML = 'Войти';
+
+  const loginPageLinkWrapper = document.createElement('div');
+  loginPageLinkWrapper.classList.add('login-link');
+
+  const loginPageLink = document.createElement('a');
+  loginPageLink.href = './register';
+  loginPageLink.innerHTML = `<i class="fa-solid fa-address-card"></i> Регистрация`;
+  loginPageLinkWrapper.append(loginPageLink);
+
+  loginBoxBodyRow.append(loginBoxBodyCheckboxWrapper, loginBoxBodyButton);
+  loginBoxBody.append(loginBoxBodyRow, loginPageLinkWrapper);
 
   loginBoxH3.append(loginBoxLink);
   loginLogoLink.append(loginLogoImg);
