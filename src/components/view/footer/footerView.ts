c9deb9b1d1a0845085ca8bcd5956932000c0534d
TerebinovSergey <neznacomec94@gmail.com>
2023-02-02 21:09:45 +0400

    feat: add adaptive for footer

--- src/components/view/footer/footerView.ts
+++ src/components/view/footer/footerView.ts
@@ -11,8 +11,8 @@ export default class FooterView {
       <div class="footer-wrapper">
         <span class="footer__year">© 2023</span>
         <div class="footer__authors">
-          <a class="footer__author" href="https://github.com/TerebinovSergey">Terebinov Sergey</a> | 
-          <a class="footer__author" href="https://github.com/Mary190183">Mary Petrovskaya</a> | 
+          <a class="footer__author" href="https://github.com/TerebinovSergey">Sergey Terebinov</a>
+          <a class="footer__author" href="https://github.com/Mary190183">Mary Petrovskaya</a>
           <a class="footer__author" href="https://github.com/alexbrestby">Alexander Leoniuk</a>
         </div>
         <a href="https://rs.school/js/" class="footer__link">
