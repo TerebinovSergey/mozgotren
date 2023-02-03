2a02f978fa2f0646930b8f2a40fe32c00ea73226
Alex Leoniuk <alex.brest.by@gmail.com>
2023-02-03 18:25:28 +0300

    feat: form validation add

--- src/components/controller/routerForDel.ts
+++ src/components/controller/routerForDel.ts
@@ -1,5 +1,4 @@
 import { getElement } from '../../utils/utils';
-import { loginPage } from '../../pages/login';
 
 type Routes = {
   [key: string]: () => void,
@@ -17,9 +16,6 @@ const routes: Routes = {
   },
   '/trenagors': () => {
     getElement('#content').innerHTML = 'Trenagors';
-  },
-  '/login': () => {
-    loginPage();
   },
 };
 
