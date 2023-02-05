8018122c2137add2f13a013896fc69db08601a5b
Alex Leoniuk <alex.brest.by@gmail.com>
2023-02-05 12:46:16 +0300

    fix: link to web server change

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,8 +11,8 @@ export function getElement(
 
 export const test: number = 0;
 
-export const baseUrl = 'http://localhost:5000';
-// export const baseUrl = 'https://api.leoniuk.dev';
+// export const baseUrl = 'http://localhost:5000';
+export const baseUrl = 'https://api.leoniuk.dev';
 
 export const submitForm = async (objValues: any) => {
   const path = (Object.keys(objValues).length === 2) ? 'login' : 'registration';
