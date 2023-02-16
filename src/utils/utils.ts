97733af703fc236bfd3312ab2560eb7e48450b4a
TerebinovSergey <106994156+TerebinovSergey@users.noreply.github.com>
2023-02-16 17:55:15 +0400

    fix: change baseUrl to api.leoniuk.dev

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -70,8 +70,8 @@ export function getRandomColor(): string {
   return `#${color}`;
 }
 
-export const baseUrl = 'http://localhost:5000';
-// export const baseUrl = 'https://api.leoniuk.dev';
+// export const baseUrl = 'http://localhost:5000';
+export const baseUrl = 'https://api.leoniuk.dev';
 
 export const submitForm = async (objValues: AuthData) => {
   const path = (Object.keys(objValues).length === 2) ? 'login' : 'registration';
